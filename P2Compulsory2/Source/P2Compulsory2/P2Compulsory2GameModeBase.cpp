75c82f47189aa056e204a945e208c9194883fffd
Henno98 <259554@stud.inn.no>
2023-03-07 17:10:54 +0100

    created gamemode

--- P2Compulsory2/Source/P2Compulsory2/P2Compulsory2GameModeBase.cpp
+++ P2Compulsory2/Source/P2Compulsory2/P2Compulsory2GameModeBase.cpp
@@ -3,3 +3,92 @@
 
 #include "P2Compulsory2GameModeBase.h"
 
+AP2Compulsory2GameModeBase::AP2Compulsory2GameModeBase()
+{
+
+	PrimaryActorTick.bCanEverTick = true;
+	WaveSize.Add(10);
+	WaveSize.Add(15);
+	WaveSize.Add(20);
+	WaveSize.Add(25);
+
+	WaveDifficulty.Add(0.5);
+	WaveDifficulty.Add(1);
+	WaveDifficulty.Add(1.5);
+	WaveDifficulty.Add(2);
+
+	WaveSpawnFrequency.Add(2);
+	WaveSpawnFrequency.Add(1.5);
+	WaveSpawnFrequency.Add(1);
+	WaveSpawnFrequency.Add(0.5);
+
+
+
+	minX = 2000;
+	maxX = 2200;
+
+	minY = -1000;
+	maxY = 1000;
+
+	leftXmin = -2000;
+	leftXmax = -2200;
+	behindYmin = -4000;
+	behindYmax = -3500;
+
+
+}
+
+void AP2Compulsory2GameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+	CurrentWave = 1;
+	LefttoSpawn = WaveSize[CurrentWave - 1];
+
+}
+
+void AP2Compulsory2GameModeBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	clock += DeltaTime;
+
+	if (clock > WaveSpawnFrequency[CurrentWave - 1])
+	{
+		clock = 0.f;
+		FVector location = FVector(FMath::RandRange(minX, maxX), FMath::RandRange(minY, maxY), 90);
+		AActor* Actor = GetWorld()->SpawnActor<AActor>(Target_BP, location, FRotator::ZeroRotator);
+		ATarget* Target1 = Cast<ATarget>(Actor);
+		Target1->Enemyspeed *= WaveDifficulty[CurrentWave - 1];
+		
+		if (LefttoSpawn <= 0)
+		{
+			ChangeWave(CurrentWave + 1);
+		}
+
+		/*if(LefttoSpawn <= 0)
+		{
+			ChangeWave(CurrentWave + 1);
+		}*/
+
+
+	}
+	if (clock > WaveSpawnFrequency[CurrentWave - 1])
+	{
+
+
+
+	}
+
+
+
+}
+void AP2Compulsory2GameModeBase::ChangeWave(int wave)
+{
+	if (WaveSize.Num() < wave)
+	{
+		return;
+
+	}
+	CurrentWave++;
+	LefttoSpawn = WaveSize[CurrentWave - 1];
+
+}
--- P2Compulsory2/Source/P2Compulsory2/P2Compulsory2GameModeBase.h
+++ P2Compulsory2/Source/P2Compulsory2/P2Compulsory2GameModeBase.h
@@ -6,12 +6,81 @@
 #include "GameFramework/GameModeBase.h"
 #include "P2Compulsory2GameModeBase.generated.h"
 
-/**
- * 
- */
+
+class ATarget;
+ 
+struct Wave
+{
+	Wave(int s, int d)
+	{
+		Size = s;
+		Difficulty = d;
+	}
+	int Size;
+	int Difficulty;
+};
 UCLASS()
 class P2COMPULSORY2_API AP2Compulsory2GameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
 	
+	
+	/**
+	 *
+	 */
+	UCLASS()
+		
+		GENERATED_BODY()
+	public:
+		AP2Compulsory2GameModeBase();
+	protected:
+		virtual void BeginPlay() override;
+	public:
+		virtual void Tick(float DeltaTime) override;
+
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables")
+			TArray<int> WaveSize;
+
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables")
+			TArray<float> WaveDifficulty;
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables")
+			TArray<int> WaveSpawnFrequency;
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables")
+			float minX;
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables")
+			float maxX;
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables")
+			float minY;
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables")
+			float maxY;
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables")
+			int CurrentWave;
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables")
+			float leftXmin;
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables")
+			float leftXmax;
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables")
+			float behindYmin;
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables")
+			float behindYmax;
+
+
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables")
+			TSubclassOf<ATarget> Target_BP;
+
+		void ChangeWave(int wave);
+
+	private:
+		int LefttoSpawn;
+		float clock;
+		float spawnfrequency;
+
+
+
+
+
+
+
+	
+
 };
