d70174df3c58dd3ea1365e783a90d50f4d3894b6
Tmmrvg <91119956+Tmmrvg@users.noreply.github.com>
2023-03-07 12:46:24 +0100

    Created project

---
+++ P2Compulsory2/Source/P2Compulsory2/P2Compulsory2GameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "P2Compulsory2GameModeBase.h"
+
---
+++ P2Compulsory2/Source/P2Compulsory2/P2Compulsory2GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "P2Compulsory2GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class P2COMPULSORY2_API AP2Compulsory2GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
